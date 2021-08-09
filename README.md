a6a761d5528a736dd17770e1ce6c8f7c18c6a9f4
vertskater <vertskater@gmx.at>
2021-08-09 12:20:45 +0200

    created readme for npm puplushing

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# dropdownmenu
+# What is this?
+
+A Drop-Down-Menu toogle with animation
+
+# Installation
+
+`npm install dropdownmenu --save`
+
+Then ...
+
+```
+import { dropdownmenu } from 'dropdownmenu';
+
+dropdownmenu(duration);
+```
+
+html ...
+
+```
+<nav>
+  <em class="nav-trigger"></em>
+  <ul>
+    <li>...</li>
+    <li>...</li>
+    <li>...</li>
+    <li>...</li>
+    <li>...</li>
+    ...
+  </ul>
+</nav>
+```
+
+## Options
+
+duration = ineger in seconds (s) or (ms);
---
+++ index.js
@@ -0,0 +1,28 @@
+import "./style.css";
+
+export default function dropdownmenu(duration) {
+  const trigger = document.querySelectorAll(".nav-trigger");
+  const menus = document.querySelectorAll("nav > ul");
+
+  menus.forEach((menu) => {
+    menu.style.opacity = "0";
+    menu.style.transform = "scale(0)";
+    menu.style.position = "absolute";
+    menu.style.width = "100%";
+    menu.style.transition = "all " + duration + " ease-in-out";
+  });
+
+  trigger.forEach((item) => {
+    item.style.cursor = "pointer";
+    item.addEventListener("click", () => {
+      menus.forEach((menu) => {
+        menu.style.opacity === "0"
+          ? (menu.style.opacity = "1")
+          : (menu.style.opacity = "0");
+        menu.style.transform === "scale(0)"
+          ? (menu.style.transform = "scale(1)")
+          : (menu.style.transform = "scale(0)");
+      });
+    });
+  });
+}
